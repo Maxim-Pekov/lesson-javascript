bfcfe9a9bd7580bd02fd48d51528477a00c077f3
Maxim <fenixx-max@mail.ru>
2020-05-24 23:23:29 +0300

    Это 6й коммит, выполнял 3 вида циклов для работы с данными от пользователя

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 "use strict"
 
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату, yyyy-mm-dd", "");
-let nameExpenses = prompt("Введите обязательную статью расходов", "");
-let moneyExpenses = prompt("Во сколько обойдется?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
+let date = prompt("Введите дату, yyyy-mm-dd", "");
 
-
-let expenses ={
-   nameExpenses :  moneyExpenses,
-};
-
-expenses[nameExpenses] = moneyExpenses;
-
-let optionalExpenses ={};
-
-var appdata = {
+let appdata = {
    money : money,
-   timedata : time,      
+   timedata : date,  
+   expenses :  {},    
+   optionalExpenses: {},
    income :  [],
    savings : false,
 };
 
-alert("Твой бюджет на один день = " + ((appdata.money-expenses[nameExpenses])/30));
+// for ( let i = 0; i < 2; i++ ){
+//    let a = prompt("Введите обязательную статью расходов", ""),
+//        b = prompt("Во сколько обойдется?", "");
+
+//    if ( (typeof(a))==="string" && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 && b.length < 10 ) {
+//       appdata.expenses[a] = b;
+//       console.log('done');
+//    }else{
+//       i--;
+//       continue;      
+//    }
+// };
+// var i = 0;
+// do {
+   
+//    let a = prompt("Введите обязательную статью расходов", ""),
+//        b = prompt("Во сколько обойдется?", "");
+
+//    if ( (typeof(a))==="string" && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 && b.length < 10 ) {
+//       appdata.expenses[a] = b;
+//       console.log('done');
+//       i++;
+//    }else{      
+//       continue;      
+//    }
+// } while ( i < 2);
+
+let i = 0;
+while ( i < 2){
+   let a = prompt("Введите обязательную статью расходов", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ( (typeof(a))==="string" && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 && b.length < 10 ) {
+       appdata.expenses[a] = b;
+       console.log('done');
+       i++;
+    }else{      
+       continue;      
+    }
+}
 
 
+
+appdata.perDay = appdata.money/30;
+alert("Твой бюджет на один день = " + (appdata.perDay/30));
 
