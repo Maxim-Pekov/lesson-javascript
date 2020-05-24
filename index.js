331ad097a28cc3199eb53035adcf213f2a4d0301
Maxim <fenixx-max@mail.ru>
2020-05-24 18:29:08 +0300

    Это 5й коммит, задание к уроку

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+"use strict"
+
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату, yyyy-mm-dd", "");
+let nameExpenses = prompt("Введите обязательную статью расходов", "");
+let moneyExpenses = prompt("Во сколько обойдется?", "");
 
 
-var a = 55;
+let expenses ={
+   nameExpenses :  moneyExpenses,
+};
 
-console.log(a);
+expenses[nameExpenses] = moneyExpenses;
 
- 
+let optionalExpenses ={};
+
+var appdata = {
+   money : money,
+   timedata : time,      
+   income :  [],
+   savings : false,
+};
+
+alert("Твой бюджет на один день = " + ((appdata.money-expenses[nameExpenses])/30));
+
+
+
